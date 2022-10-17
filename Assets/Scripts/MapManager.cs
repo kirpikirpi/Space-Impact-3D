0f3829506e561ca617e8ffe63733cfe523506ecb
roman <roman.safron@aon.at>
2022-10-17 12:38:33 +0200

    feat: map generator
    builds a basic tunnel

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -7,8 +7,12 @@ public class MapManager : MonoBehaviour
     public GameObject PlayerGameObject;
     public GameObject EnemyGameObject;
 
+    public GameObject MapSegmentPrefab;
+
     private float tunnelRadius = 15f;
     private float tunnelLength = 100f;
+    private float unitsBetweenCompleteSegments = 2f;
+    private float segmentSpaceing = 0.1f;
     
     private int numEnemies = 5;
 
@@ -20,6 +24,15 @@ public class MapManager : MonoBehaviour
         for (int i = 0; i < numEnemies; i++)
         {
             Instantiate(EnemyGameObject, GenerateRandomPosition(), enemyRotation);
+        }
+
+        for (float i = 0; i < tunnelLength; i+= unitsBetweenCompleteSegments)
+        {
+            for (float j = 0; j < 2 * Mathf.PI; j += segmentSpaceing)
+            {
+                Vector3 segmentPos = new Vector3(Mathf.Cos(j) * tunnelRadius, Mathf.Sin(j) * tunnelRadius, i);
+                Instantiate(MapSegmentPrefab, segmentPos, Quaternion.identity);
+            }
         }
     }
 
